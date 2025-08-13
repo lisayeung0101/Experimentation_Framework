3cf889ad43536b1a3c0cf076da55c9bfe0820445
Lisa Yeung <lisahyyeung@gmail.com>
2025-08-12 21:10:13 -0700

    Cuped adjustment script

---
+++ src/cuped.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def cuped_adjust(y, theta):
+    cov = np.cov(y, theta, ddof=1)
+    var = np.var(theta, ddof=1)
+    if var == 0:
+        return y, 0.0
+    c = cov / var
+    y_adj = y - c * theta 
+    return y_adj, c
